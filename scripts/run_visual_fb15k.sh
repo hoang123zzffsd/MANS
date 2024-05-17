3758cb1ad8153e0287839dc7c15b201c6f8f6999
hoang123zzffsd <88784062+hoang123zzffsd@users.noreply.github.com>
2024-05-17 20:29:23 +0700

    Update run_visual_fb15k.sh

--- scripts/run_visual_fb15k.sh
+++ scripts/run_visual_fb15k.sh
@@ -4,9 +4,9 @@ CUDA_VISIBLE_DEVICES=0 nohup python run.py -dataset=FB15K \
   -neg_mode=img \
   -train_mode=normal \
   -epoch=1000 \
-  -save=./checkpoint/FB15K-img \
+  -save=/content/MANS/checkpoint/FB15K-img \
   -test_mode=lp \
   -img_grad=True \
   -kernel=transe \
   -neg_num=1 \
-  -img_dim=4096 > ./log/FB15K-TransE-img.txt &
+  -img_dim=4096 > /content/MANS/log/FB15K-TransE-img.txt &
