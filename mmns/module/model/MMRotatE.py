e81fd9171580b1af54eaa9bea17789468e7604a2
hoang123zzffsd <88784062+hoang123zzffsd@users.noreply.github.com>
2024-05-27 21:34:41 +0700

    Update MMRotatE.py

--- mmns/module/model/MMRotatE.py
+++ mmns/module/model/MMRotatE.py
@@ -7,19 +7,23 @@ from .Model import Model
 class MMRotatE(Model):
 
     def __init__(self, ent_tot, rel_tot, dim=100, img_emb=None, img_dim=4096,
-                 norm_flag=True, margin=None, epsilon=None, test_mode='lp', beta=None):
+                 margin=None, epsilon=None, test_mode='lp', beta=None):
         super(MMRotatE, self).__init__(ent_tot, rel_tot)
 
-        self.dim = dim // 2  # Use half dimension for complex number representation
+
+        self.dim_e = dim * 2
+        self.dim_r = dim
+
+                     
         self.margin = margin
         self.epsilon = epsilon
-        self.norm_flag = norm_flag
         self.img_dim = img_dim
         self.test_mode = test_mode
 
-        self.ent_embeddings = nn.Embedding(self.ent_tot, self.dim * 2)
-        self.rel_embeddings = nn.Embedding(self.rel_tot, self.dim)
-        self.img_proj = nn.Linear(self.img_dim, self.dim * 2)
+
+        self.ent_embeddings = nn.Embedding(self.ent_tot, self.dim_e)
+        self.rel_embeddings = nn.Embedding(self.rel_tot, self.dim_r)
+        self.img_proj = nn.Linear(self.img_dim, self.dim_e)
         self.img_embeddings = img_emb
         self.img_embeddings.requires_grad = False
         self.beta = beta
@@ -28,53 +32,64 @@ class MMRotatE(Model):
             nn.init.xavier_uniform_(self.ent_embeddings.weight.data)
             nn.init.xavier_uniform_(self.rel_embeddings.weight.data)
         else:
-            self.embedding_range = nn.Parameter(
-                torch.Tensor([(self.margin + self.epsilon) / self.dim]), requires_grad=False
-            )
+            self.ent_embedding_range = nn.Parameter(
+			torch.Tensor([(self.margin + self.epsilon) / self.dim_e]), 
+			requires_grad=False
+		    )
             nn.init.uniform_(
-                tensor=self.ent_embeddings.weight.data,
-                a=-self.embedding_range.item(),
-                b=self.embedding_range.item()
-            )
+			tensor = self.ent_embeddings.weight.data, 
+			a=-self.ent_embedding_range.item(), 
+			b=self.ent_embedding_range.item()
+		)
+            self.rel_embedding_range = nn.Parameter(
+			torch.Tensor([(self.margin + self.epsilon) / self.dim_r]), 
+			requires_grad=False
+		)
             nn.init.uniform_(
-                tensor=self.rel_embeddings.weight.data,
-                a=-self.embedding_range.item(),
-                b=self.embedding_range.item()
-            )
+			tensor = self.rel_embeddings.weight.data, 
+			a=-self.rel_embedding_range.item(), 
+			b=self.rel_embedding_range.item()
+		)
 
-        if margin is not None:
-            self.margin = nn.Parameter(torch.Tensor([margin]))
-            self.margin.requires_grad = False
-            self.margin_flag = True
-        else:
-            self.margin_flag = False
+
+        self.margin = nn.Parameter(torch.Tensor([margin]))
+        self.margin.requires_grad = False
 
     def _calc(self, h, t, r, mode):
-        pi = 3.14159265358979323846
+        pi = self.pi_const
         re_head, im_head = torch.chunk(h, 2, dim=-1)
         re_tail, im_tail = torch.chunk(t, 2, dim=-1)
-        phase_relation = r / (self.embedding_range.item() / pi)
+
+        phase_relation = r / (self.rel_embedding_range.item() / pi)
 
         re_relation = torch.cos(phase_relation)
         im_relation = torch.sin(phase_relation)
+        
+        re_head = re_head.view(-1, re_relation.shape[0], re_head.shape[-1]).permute(1, 0, 2)
+        re_tail = re_tail.view(-1, re_relation.shape[0], re_tail.shape[-1]).permute(1, 0, 2)
+        im_head = im_head.view(-1, re_relation.shape[0], im_head.shape[-1]).permute(1, 0, 2)
+        im_tail = im_tail.view(-1, re_relation.shape[0], im_tail.shape[-1]).permute(1, 0, 2)
+        im_relation = im_relation.view(-1, re_relation.shape[0], im_relation.shape[-1]).permute(1, 0, 2)
+        re_relation = re_relation.view(-1, re_relation.shape[0], re_relation.shape[-1]).permute(1, 0, 2)
 
-        re_score = re_head * re_relation - im_head * im_relation
-        im_score = re_head * im_relation + im_head * re_relation
+		
+        
+        if mode == "head_batch":
+            re_score = re_relation * re_tail + im_relation * im_tail
+            im_score = re_relation * im_tail - im_relation * re_tail
+            re_score = re_score - re_head
+            im_score = im_score - im_head
+        else:
+            re_score = re_head * re_relation - im_head * im_relation
+            im_score = re_head * im_relation + im_head * re_relation
+            re_score = re_score - re_tail
+            im_score = im_score - im_tail
+        
+        score = torch.stack([re_score, im_score], dim = 0)
+        score = score.norm(dim = 0).sum(dim = -1)
+        return score.permute(1, 0).flatten()
 
-        if mode != 'normal':
-            re_score = re_score.view(-1, r.shape[0], re_score.shape[-1])
-            im_score = im_score.view(-1, r.shape[0], im_score.shape[-1])
-            re_tail = re_tail.view(-1, r.shape[0], re_tail.shape[-1])
-            im_tail = im_tail.view(-1, r.shape[0], im_tail.shape[-1])
-
-        re_score = re_score - re_tail
-        im_score = im_score - im_tail
-
-        score = torch.stack([re_score, im_score], dim=0)
-        score = score.norm(dim=0)
-        score = score.sum(dim=-1).flatten()
-        return score
-
+    
     def forward(self, data, batch_size, neg_mode='normal', neg_num=1):
         batch_h = data['batch_h']
         batch_t = data['batch_t']
@@ -121,16 +136,13 @@ class MMRotatE(Model):
         r = self.rel_embeddings(batch_r)
         h_img_emb = self.img_proj(self.img_embeddings(h_img))
         t_img_emb = self.img_proj(self.img_embeddings(t_img))
-        score = (
+        score = self.margin - (
                 self._calc(h, t, r, mode)
                 + self._calc(h_img_emb, t_img_emb, r, mode)
                 + self._calc(h_img_emb, t, r, mode)
                 + self._calc(h, t_img_emb, r, mode)
         )
-        if self.margin_flag:
-            return self.margin - score
-        else:
-            return score
+        return score
 
     def regularization(self, data):
         batch_h = data['batch_h']
@@ -155,22 +167,17 @@ class MMRotatE(Model):
         r = self.rel_embeddings(batch_r)
         h_img_emb = self.img_proj(self.img_embeddings(h_img))
         t_img_emb = self.img_proj(self.img_embeddings(t_img))
-        score = self._calc(h, t_img_emb, r, mode)
-        if self.margin_flag:
-            return self.margin - score
-        else:
-            return score
-
+        score = self.margin - self._calc(h ,t_img_emb, r, mode)
+        return score
+ 
     def predict(self, data):
         if self.test_mode == 'cmlp':
             score = self.cross_modal_score_ent2img(data)
         else:
             score = self.forward(data, batch_size=None, neg_mode='normal')
-        if self.margin_flag:
-            score = self.margin - score
-            return score.cpu().data.numpy()
-        else:
-            return score.cpu().data.numpy()
+        score = -self.forward(data)
+        return score.cpu().data.numpy()
+
 
     def set_test_mode(self, new_mode):
         self.test_mode = new_mode
